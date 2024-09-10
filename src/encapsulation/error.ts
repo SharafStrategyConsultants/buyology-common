3c95fd41481d60ee396a200a5e561567246b9ac5
ridhav30 <ridhav30@gmail.com>
2024-09-11 01:32:55 +0530

    added data passing as need

--- src/encapsulation/error.ts
+++ src/encapsulation/error.ts
@@ -1,10 +1,10 @@
 export class CustomError extends Error {
   statusCode: number;
-
-  constructor(message: string, statusCode: number) {
+  data: any;
+  constructor(message: string, statusCode: number, data: any) {
     super(message);
     this.statusCode = statusCode;
-
+    this.data = data;
     if (typeof (Error as any).captureStackTrace === "function") {
       (Error as any).captureStackTrace(this, this.constructor);
     } else {
